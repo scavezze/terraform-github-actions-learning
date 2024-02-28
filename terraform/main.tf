784ad8dd2ade448b6456904886ffc7fa61e19770
Nathan Scavezze <nscavezze@cobank.com>
2024-02-28 12:02:46 -0700

    test to see if i can create stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,12 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
